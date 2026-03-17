1b5e4b38f9dea18f9d868958cbb258bcfc30ab46
Mattis <mattis.philippe@epitech.eu>
2026-03-17 11:35:01 +0100

    test lecture d'un livre télécharger depuis Gutenberg Project

--- bookworm.py
+++ bookworm.py
@@ -1,1 +1,9 @@
-"Yop tout le monde c'est Gotaga "
+with open("pg84.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+
+start = text.find("*** START OF")
+end = text.find("*** END OF")
+
+clean_text = text[start:end].splitlines()[1:]  
+
+print("\n".join(clean_text))

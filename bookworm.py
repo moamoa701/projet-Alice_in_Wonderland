5ac87f6d1c55bceaff2449fe7e5a45fd3cbab8ab
Mattis <mattis.philippe@epitech.eu>
2026-03-18 10:27:39 +0100

    fonction pour dowload des livres depuis le lines gutenberg

--- bookworm.py
+++ bookworm.py
@@ -1,9 +1,18 @@
-with open("pg84.txt", "r", encoding="utf-8") as f:
-    text = f.read()
+import requests
 
-start = text.find("*** START OF")
-end = text.find("*** END OF")
+url = "https://www.gutenberg.org/"
 
-clean_text = text[start:end].splitlines()[1:]  
+requests.get(url)
 
-print("\n".join(clean_text))
+
+
+def clean_gutenberg_text(text):
+    "permet de nettoyer le text téléchargé"
+
+
+    start = text.find("*** START OF")
+    end = text.find("*** END OF")
+    
+    clean_text = text[start:end].splitlines()[1:]
+    
+    return "\n".join(clean_text)
